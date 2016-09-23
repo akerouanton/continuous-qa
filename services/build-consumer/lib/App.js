33349dab3470838b3be816d063199181b755a9bf
Albin Kerouanton <albin.kerouanton@knplabs.com>
2016-09-23 12:33:52 +0200

    Update build status when runner is started

--- services/build-consumer/lib/App.js
+++ services/build-consumer/lib/App.js
@@ -4,49 +4,50 @@ const logger = require('tracer').colorConsole();
 const Promise = require('promise');
 const request = require('request');
 const post = Promise.denodeify(request.post);
+const patch = Promise.denodeify(request.patch);
 
 import Consumer from './Consumer';
 
 export default class {
   constructor(config) {
-    const emitter = new EventEmitter();
-    this._consumer = new Consumer(emitter, config.amqp);
+    this._emitter = new EventEmitter();
+    this._consumer = new Consumer(this._emitter, config.amqp);
+    this._config = config;
 
-    emitter.on('buildCreated', (event) => {
+    this._emitter.on('buildCreated', (event) => {
       const buildUrn = event.urn || null;
       const analyses = event.analyses || null;
       const repoUrl  = event.repoUrl || null;
+      const reference = event.reference || null;
 
-      if (buildUrn === null || analyses === null || repoUrl === null) {
+      if (buildUrn === null || analyses === null || repoUrl === null || reference === null) {
         const err = {
-          message: 'Message was incomplete (either urn, analyses or repoUrl were missing)/',
+          message: 'Message was incomplete (either urn, analyses, repoUrl or reference were missing).',
           event: event,
           data: {
             buildUrn: buildUrn,
             analyses: analyses,
-            repoUrl: repoUrl
+            repoUrl: repoUrl,
+            reference: reference
           }
         };
-        emitter.emit('error', err);
 
+        this._emitter.emit('error', err);
         return;
       }
 
-      const promises = [];
-
-      for (let analysis of analyses) {
-        var url = config.runnerEndpoint + encodeURIComponent(buildUrn) + '/' + analysis['analyzer'] + '/start';
-          promises.push(post({url: url, form: {repoUrl: repoUrl}}));
-      }
+      const promises = analyses.map((analysis) => {
+        return this._startRunner(buildUrn, analysis, repoUrl, reference);
+      });
 
       Promise
         .all(promises)
         .then(() => {
-          logger.info('Runners created.', {analyses: analyses});
-          emitter.emit('runnersCreated');
+          logger.info('Runners started.', {analyses: analyses});
+          this._emitter.emit('runnersStarted');
         })
         .catch((err) => {
-          emitter.emit('error', err);
+          this._emitter.emit('error', err);
         })
       ;
     });
@@ -54,5 +55,44 @@ export default class {
 
   run() {
     this._consumer.connect();
+  }
+
+  _startRunner(buildUrn, analysis, repoUrl, reference) {
+    const analyzer = analysis.analyzer;
+    const url = `${this._config.runnerEndpoint}/${encodeURIComponent(buildUrn)}/${analyzer}/start`;
+
+    return post({url: url, form: {repoUrl: repoUrl, reference: reference}})
+      .then((response) => {
+        if (response.statusCode !== 200) {
+          throw new Error(
+            'An error occurred while starting a new runner.',
+            { buildUrn: buildUrn, url: url, analyzer: analyzer, statusCode: response.statusCode, body: response.body }
+          );
+        }
+
+        logger.debug(`Runner for "${buildUrn}:${analyzer}" started.`);
+
+        return this._updateBuildStatus(buildUrn, analyzer);
+      })
+    ;
+  }
+
+  _updateBuildStatus(buildUrn, analyzer) {
+    const url = `${this._config.buildEndpoint}/${encodeURIComponent(buildUrn)}/${analyzer}`;
+
+    logger.info('Updating analysis status.', {buildUrn: buildUrn, analyzer: analyzer, url: url});
+
+    return patch({url: url, form: {state: 'running'}})
+      .then((response) => {
+        if (response.statusCode !== 200) {
+          throw new Error(
+            'An error occurred while updating build status.',
+            { buildUrn: buildUrn, url: url, statusCode: response.statusCode, body: response.body }
+          );
+        }
+
+        logger.debug('Build status updated.', {buildUrn: buildUrn, analyzer: analyzer});
+      })
+    ;
   }
 }
