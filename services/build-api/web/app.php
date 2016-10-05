52915744af9882428808e3d393928e869fa824f8
Albin Kerouanton <albin.kerouanton@knplabs.com>
2016-10-05 04:55:22 +0200

    Remove old Build API, written in PHP

--- services/build-api/web/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require __DIR__.'/../vendor/autoload.php';
-
-$config = require __DIR__.'/../config.php';
-$debug  = getenv('DEBUG') === 'true';
-
-(new \App\Application($debug, $config))->run();
